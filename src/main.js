c3a134d17c120c0df6801c74b103fff5fc02865f
cramosgamboa <c.ramosgamboa@gmail.com>
2021-11-16 12:35:07 -0300

    template menu

--- src/main.js
+++ src/main.js
@@ -6,6 +6,18 @@
 import { register } from './view/register.js';
 
 myFunction();
+<<<<<<< HEAD
 //document.getElementById("root").innerHTML = menu();
 //document.getElementById("root").appendChild(login());
 document.getElementById("root").innerHTML = register();
+=======
+//import { myFunction } from './lib/index.js';
+//import { menu } from './view/templeteMenu.js';
+import { login } from './view/login.js';
+//import { register } from './view/register.js';
+
+myFunction();
+//document.getElementById("root").innerHTML = menu();
+document.getElementById("root").appendChild(login());
+//document.getElementById("root").appendChild(register());
+>>>>>>> 82e351b49453d36bfac1d7a355a1f38560d288b2
--- src/view/register.js
+++ src/view/register.js
@@ -1,27 +1,23 @@
-import { signInEmail } from "../lib/firebase.js";
 export const register = () => {
-  const formRegister = document.createElement("section");
-  const viewRegister = `
+    const formRegister = document.createElement("section");
+    const viewRegister = `
     <img src="/img/logo.png" class="logo" alt="Logo Sport Fem">
     <div class="container">
     <form class="login">
     <section>
-        <input type="text"class="form" id="name"
+        <input type="text"class="form" id="email"
           placeholder="Ingresa tu nombre"/>
         </section>
         <section>
-          <input type="text"class="form" id="email"
+          <input type="text"class="form" id="password"
           placeholder="Ingresa tu email"/>
           </section>
           <section>
-        <input type="text" class="form" id="password"
+        <input type="text" class="form"
           placeholder="Ingresa tu contraseña"/>
           </section>
           <button class="formBtn">Registrar</button>`
 
-  formRegister.innerHTML = viewRegister;
-  containerLogin.querySelector("#formBtn").addEventListener("click", () => {
-    signInEmail();
-  });
-  return formRegister;
+formRegister.innerHTML = viewRegister;
+return formRegister;
 };
